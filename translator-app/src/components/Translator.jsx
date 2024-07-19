f675384828f9564ced42b6f1471b060a309d1c2c
AZIM BEG <118754891+azimbeg@users.noreply.github.com>
2024-07-19 16:30:13 +0000

    update

--- translator-app/src/components/Translator.jsx
+++ translator-app/src/components/Translator.jsx
@@ -1,11 +1,26 @@
+import { useState } from "react";
 import languages from "./languages";
 const Translator =() =>{
+    const [fromText, setFromText] = useState('');
+    const [toText, setToText] = useState('');
+    const [fromLanguage, setFromLanguage] = useState('en-GB');
+    const [toLanguage, setToLanguage] = useState('hi-IN');
+
+    const handleExchange =()=>{
+        let tempValue = fromText;
+        setFromText(toText);
+        setToText(tempValue);
+
+        let tempLang = fromLanguage;
+        setFromLanguage(toLanguage);
+        setToLanguage(tempLang);
+    }
     return(
         <>
             <div className="wrapper">
                 <div className="text-input">               
-                    <textarea name="from" className="from-text" id="from" placeholder="Enter Text"></textarea>
-                    <textarea name="to" className="to-text" id="to" readOnly placeholder="enter text"></textarea>
+                    <textarea name="from" className="from-text" id="from" placeholder="Enter Text" value={fromText} onChange={(event)=>setFromText(event.target.value) }></textarea>
+                    <textarea name="to" className="to-text" id="to" readOnly value={toText}></textarea>
                 </div>
                 <ul className="controls">
                     <li className="row from">
@@ -13,7 +28,7 @@ const Translator =() =>{
                         <i id="from" class="fas fa-volume-high"></i>
                         <i id="from" class="fas fa-copy"></i>
                         </div>
-                        <select>
+                        <select value={fromLanguage} onChange={(event)=>setFromLanguage(event.target.value)}>
                             {
                                 Object.entries(languages).map(([code, name])=>{
                                     return <option key={code} value={code}>{name}</option>
@@ -21,11 +36,11 @@ const Translator =() =>{
                             }
                         </select>
                     </li>
-                    <li className="exchange">
+                    <li className="exchange" onClick={handleExchange}>
                     <i class="fa-solid fa-arrow-right-arrow-left"></i>
                     </li>
                     <li className="row to">
-                        <select>
+                        <select value={toLanguage} onChange={(event)=>setToLanguage(event.target.value)}>
                         {
                                 Object.entries(languages).map(([code, name])=>{
                                     return <option key={code} value={code}>{name}</option>
