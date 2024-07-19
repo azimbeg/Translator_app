fcd1fe5c299a7dbb6484f67aaa1dcc6234301b4f
AZIM BEG <118754891+azimbeg@users.noreply.github.com>
2024-07-19 15:04:16 +0000

    add new file translator

---
+++ translator-app/src/App.js
@@ -0,0 +1,12 @@
+import Translator from './components/Translator';
+import './App.css';
+
+function App() {
+  return (
+    <div className='container'>
+      <Translator/>
+    </div>
+  );
+}
+
+export default App;
---
+++ translator-app/src/components/Translator.jsx
@@ -0,0 +1,11 @@
+const Translator =() =>{
+    return(
+        <>
+            <div>
+                <p>Hello</p>
+            </div>
+        </>
+    );
+}
+
+export default Translator;
