9e163cc538a11e9d6743881f2166b0872f2da3f0
AZIM BEG <118754891+azimbeg@users.noreply.github.com>
2024-07-23 17:58:11 +0000

    update

--- translator-app/src/components/Translator.jsx
+++ translator-app/src/components/Translator.jsx
@@ -1,12 +1,14 @@
 import { useState } from "react";
 import languages from "./languages";
+
 const Translator =() =>{
     const [fromText, setFromText] = useState('');
     const [toText, setToText] = useState('');
     const [fromLanguage, setFromLanguage] = useState('en-GB');
     const [toLanguage, setToLanguage] = useState('hi-IN');
+    const [loading, setLoading] = useState(false);
 
-    const handleExchange =()=>{
+    const handleExchange =(event)=>{
         let tempValue = fromText;
         setFromText(toText);
         setToText(tempValue);
@@ -15,6 +17,50 @@ const Translator =() =>{
         setFromLanguage(toLanguage);
         setToLanguage(tempLang);
     }
+
+    const copyContent =(text)=>{
+        navigator.clipboard.writeText(text);
+    }
+
+    const utterText =(text, languages)=>{
+        const synth = window.speechSynthesis;
+        const utterrance = new SpeechSynthesisUtterance(text);
+        utterrance.lang = languages;
+        synth.speak(utterrance);
+    }
+
+    const handleTranslate = () =>{
+        setLoading(true);
+        let url = `https://api.mymemory.translated.net/get?q=${fromText}&langpair=${fromLanguage}|${toLanguage}`;
+
+        fetch(url).then((res)=>res.json()).then((data)=>{
+            setToText(data.responseData.translatedText);
+            setLoading(false);
+        })
+    }
+
+    const handleIconClick = (target,id)=>{
+        if(target.classList.contains('fa-copy')){
+            if(id === 'from'){
+                copyContent(fromText);
+            }
+            else{
+                copyContent(toText);
+            }
+            
+        }else{
+            if(id === 'from'){
+                utterText(fromText, fromLanguage);
+            }
+            else{
+                utterText(toText, toLanguage);
+            }
+    }
+
+
+
+    }
+    
     return(
         <>
             <div className="wrapper">
@@ -25,8 +71,8 @@ const Translator =() =>{
                 <ul className="controls">
                     <li className="row from">
                         <div className="icons">
-                        <i id="from" class="fas fa-volume-high"></i>
-                        <i id="from" class="fas fa-copy"></i>
+                        <i id="from" class="fas fa-volume-high" onClick={(e) => handleIconClick(e.target, 'from')}></i>
+                        <i id="from" class="fas fa-copy" onClick={(e)=>handleIconClick(e.target,'from')}></i>
                         </div>
                         <select value={fromLanguage} onChange={(event)=>setFromLanguage(event.target.value)}>
                             {
@@ -48,15 +94,17 @@ const Translator =() =>{
                             }
                         </select>
                     <div className="icons">
-                        <i id="to" class="fas fa-copy"></i>
-                        <i id="to" class="fas fa-volume-high"></i>
+                        <i id="to" class="fas fa-copy" onClick={(e)=>handleIconClick(e.target,'to')}></i>
+                        <i id="to" class="fas fa-volume-high" onClick={(e)=>handleIconClick(e.target,'to')}></i>
                         </div>
                     </li>
                 </ul>
             </div>
-            <button>Translat</button>
+            <button onClick={handleTranslate}>{loading ? "Translating..." : "Translate Text"}</button>
         </>
     );
 }
+
+
 
 export default Translator;
